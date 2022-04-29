791c5440208856151d123c5ff90e355e3b44e26d
Kumiszhan Dybyspayeva <d.kumiszhan@gmail.com>
2022-04-29 23:19:48 +0100

    Draft CSS

--- app.js
+++ app.js
@@ -1,67 +1,85 @@
-const http = require('http');
-const fs = require('fs');
-const activeUsers = require('./active_users');
-const { Server } = require('socket.io');
+const http = require("http");
+const fs = require("fs");
+const activeUsers = require("./active_users");
+const { Server } = require("socket.io");
 
 const server = http.createServer((req, res) => {
-    console.log(JSON.stringify(req.url));
+  console.log(JSON.stringify(req.url));
 
-    switch (req.url) {
-        case '/':
-            res.writeHead(200, { 'content-type': 'text/html' });
-            fs.createReadStream('./index.html').pipe(res);
-            break;
-        case '/pages/index.css':
-            res.writeHead(200, { 'content-type': 'text/css' });
-            fs.createReadStream('./pages/index.css').pipe(res);
-            break;
-        case '/vendor/normalize.css':
-            res.writeHead(200, { 'content-type': 'text/css' });
-            fs.createReadStream('./vendor/normalize.css').pipe(res);
-            break;
-        case '/header/__title/header__title.css':
-            res.writeHead(200, { 'content-type': 'text/css' });
-            fs.createReadStream('./header/__title/header__title.css').pipe(res);
-            break;
-        case '/public/script.js':
-            res.writeHead(200, { 'content-type': 'text/javascript' });
-            fs.createReadStream('./public/script.js').pipe(res);
-            break;
-        case '':
-            break;
-        default:
-            if (req.url.startsWith('/api/active_users.json')) {
-                activeUsers.getData(req, res);
-            } else {
-                res.writeHead(404);
-                res.end('not found');
-            }
-            break;
-    }
-
-
-
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "content-type": "text/html" });
+      fs.createReadStream("./index.html").pipe(res);
+      break;
+    case "/pages/index.css":
+      res.writeHead(200, { "content-type": "text/css" });
+      fs.createReadStream("./pages/index.css").pipe(res);
+      break;
+    case "/vendor/normalize.css":
+      res.writeHead(200, { "content-type": "text/css" });
+      fs.createReadStream("./vendor/normalize.css").pipe(res);
+      break;
+    case "/header/header.css":
+      res.writeHead(200, { "content-type": "text/css" });
+      fs.createReadStream("./header/header.css").pipe(res);
+      break;
+    case "/header/__title/header__title.css":
+      res.writeHead(200, { "content-type": "text/css" });
+      fs.createReadStream("./header/__title/header__title.css").pipe(res);
+      break;
+    case "/dayview-board/dayview-board.css":
+      res.writeHead(200, { "content-type": "text/css" });
+      fs.createReadStream("./dayview-board/dayview-board.css").pipe(res);
+      break;
+    case "/historicview-board/historicview-board.css":
+      res.writeHead(200, { "content-type": "text/css" });
+      fs.createReadStream("./historicview-board/historicview-board.css").pipe(
+        res
+      );
+      break;
+    case "/public/script.js":
+      res.writeHead(200, { "content-type": "text/javascript" });
+      fs.createReadStream("./public/script.js").pipe(res);
+      break;
+    case "./script.js":
+      res.writeHead(200, { "content-type": "text/javascript" });
+      fs.createReadStream("./index.js").pipe(res);
+      break;
+    case "./script.js":
+      res.writeHead(200, { "content-type": "image/jpeg" });
+      fs.createReadStream("./images/image.jpg").pipe(res);
+      break;
+    case "":
+      break;
+    default:
+      if (req.url.startsWith("/api/active_users.json")) {
+        activeUsers.getData(req, res);
+      } else {
+        res.writeHead(404);
+        res.end("not found");
+      }
+      break;
+  }
 });
 
 const io = new Server(server, {
-    serveClient: true
+  serveClient: true,
 });
 
 io.on("connection", (socket) => {
-    // send a message to the client
-    socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) });
-    let x = 1;
-    setInterval(function () {
-        socket.emit('new-number', x);
-        x = x % 10 + 1;
-    }, 1000);
+  // send a message to the client
+  socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) });
+  let x = 1;
+  setInterval(function () {
+    socket.emit("new-number", x);
+    x = (x % 10) + 1;
+  }, 1000);
 
-
-    // receive a message from the client
-    socket.on("hello from client", (...args) => {
-        // ..
-        console.log('received hello from client: ' + JSON.stringify(args));
-    });
+  // receive a message from the client
+  socket.on("hello from client", (...args) => {
+    // ..
+    console.log("received hello from client: " + JSON.stringify(args));
+  });
 });
 
 server.listen(process.env.PORT || 3000);
--- public/script.js
+++ public/script.js
@@ -1,4 +1,3 @@
-
 const socket = io("ws://localhost:3000");
 
 // send a message to the server
@@ -6,16 +5,151 @@ socket.emit("hello from client", 5, "6", { 7: Uint8Array.from([8]) });
 
 // receive a message from the server
 socket.on("hello from server", (...args) => {
-    console.log('recieved hello from backend: ' + JSON.stringify(args));
+  console.log("recieved hello from backend: " + JSON.stringify(args));
   // ...
 });
 
-socket.on('new-number', (...args) => {
-    console.log('received new number ' + args[0]);
+// socket.on("new-number", (...args) => {
+//   console.log("received new number " + args[0]);
 
-    document.querySelector('#number').innerHTML = args[0];
-}); 
+//   document.querySelector("#number").innerHTML = args[0];
+// });
 
+console.log("Hello from frontend JS");
 
+const config = {
+  type: "bar",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Day View",
+        backgroundColor: "#2E5681",
+        borderColor: "#2E5681",
+        data: [],
+      },
+    ],
+  },
+  options: {},
+};
 
-console.log('Hello from frontend JS');
+const configHistoric = {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Historic View",
+        backgroundColor: "#FEC414",
+        borderColor: "#FEC414",
+        data: [],
+      },
+    ],
+  },
+  options: {},
+};
+
+const configHistoricCities = {
+  type: "bar",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Historic View",
+        backgroundColor: "#FEC414",
+        borderColor: "#FEC414",
+        data: [],
+      },
+    ],
+  },
+  options: {},
+};
+
+let myChart;
+let myChartHistoric;
+let myChartHistoricCities;
+
+const startDate = document.querySelector(".start-date");
+const endDate = document.querySelector(".end-date");
+
+function initChart() {
+  myChart = new Chart(document.getElementById("myChart"), config);
+  myChartHistoric = new Chart(
+    document.getElementsByClassName("historic-view"),
+    configHistoric
+  );
+  myChartHistoricCities = new Chart(
+    document.getElementsByClassName("historic-view-cities"),
+    configHistoricCities
+  );
+}
+
+initChart();
+
+const totalUsers = document.querySelector(".totalUsers");
+console.log(totalUsers);
+function updateChart(cities, activeUsers) {
+  console.log(activeUsers);
+  const usersCount = activeUsers.reduce(
+    (accumulator, curr) => accumulator + curr,
+    0
+  );
+  console.log(usersCount);
+  totalUsers.textContent = usersCount;
+  config.data.labels = cities;
+  config.data.datasets[0].data = activeUsers;
+  myChart.update();
+}
+
+startDate.addEventListener("change", function () {
+  console.log(this);
+  //startDate.value = this;
+  startDate.onchange = dateChanged(this);
+});
+
+endDate.addEventListener("change", function () {
+  endDate.onchange = dateChanged(this);
+});
+
+function collectData(dateStart, dateEnd) {
+  // let httpRequest = new XMLHttpRequest();
+  // httpRequest.open("GET", "/api/active_users.json?date=" + date, true);
+  // httpRequest.onreadystatechange = function () {
+  //   if (httpRequest.readyState === XMLHttpRequest.DONE) {
+  //     // Everything is good, the response was received.
+  //     if (httpRequest.status === 200) {
+  //       alert(httpRequest.responseText);
+  //       let parsed = JSON.parse(httpRequest.responseText);
+  //       updateChart(parsed.cities, parsed.activeUsers);
+  //     } else {
+  //       alert("There was a problem with the request.");
+  //     }
+  //   } else {
+  //     // Not ready yet.
+  //   }
+  // };
+}
+
+function dateChanged(target) {
+  console.log("date changed ", target.value);
+  let date = target.value;
+
+  let httpRequest = new XMLHttpRequest();
+  httpRequest.open("GET", "/api/active_users.json?date=" + date, true);
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      // Everything is good, the response was received.
+      if (httpRequest.status === 200) {
+        alert(httpRequest.responseText);
+        let parsed = JSON.parse(httpRequest.responseText);
+        updateChart(parsed.cities, parsed.activeUsers);
+      } else {
+        alert("There was a problem with the request.");
+      }
+    } else {
+      // Not ready yet.
+    }
+  };
+
+  httpRequest.send();
+}
