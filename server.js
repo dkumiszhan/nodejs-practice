923a3ba2009b7838a0c15ae2451c93e5d6639689
Kumiszhan Dybyspayeva <d.kumiszhan@gmail.com>
2022-04-30 00:34:57 +0100

    Switched to expressjs and disabled socketIO

--- server.js
+++ server.js
@@ -1,79 +1,13 @@
-const http = require("http");
-const fs = require("fs");
-const activeUsers = require("./active_users");
-const { Server } = require("socket.io");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log(JSON.stringify(req.url));
-
-  switch (req.url) {
-    case "/":
-      res.writeHead(200, { "content-type": "text/html" });
-      fs.createReadStream("./index.html").pipe(res);
-      break;
-    case "/pages/index.css":
-      res.writeHead(200, { "content-type": "text/css" });
-      fs.createReadStream("./pages/index.css").pipe(res);
-      break;
-    case "/vendor/normalize.css":
-      res.writeHead(200, { "content-type": "text/css" });
-      fs.createReadStream("./vendor/normalize.css").pipe(res);
-      break;
-    case "/header/header.css":
-      res.writeHead(200, { "content-type": "text/css" });
-      fs.createReadStream("./header/header.css").pipe(res);
-      break;
-    case "/header/__title/header__title.css":
-      res.writeHead(200, { "content-type": "text/css" });
-      fs.createReadStream("./header/__title/header__title.css").pipe(res);
-      break;
-    case "/dayview-board/dayview-board.css":
-      res.writeHead(200, { "content-type": "text/css" });
-      fs.createReadStream("./dayview-board/dayview-board.css").pipe(res);
-      break;
-    case "/historicview-board/historicview-board.css":
-      res.writeHead(200, { "content-type": "text/css" });
-      fs.createReadStream("./historicview-board/historicview-board.css").pipe(
-        res
-      );
-      break;
-    case "/public/script.js":
-      res.writeHead(200, { "content-type": "text/javascript" });
-      fs.createReadStream("./public/script.js").pipe(res);
-      break;
-    case "/images/image.jpg":
-      res.writeHead(200, { "content-type": "image/jpeg" });
-      fs.createReadStream("./images/image.jpg").pipe(res);
-      break;
-    default:
-      if (req.url.startsWith("/api/active_users.json")) {
-        activeUsers.getData(req, res);
-      } else {
-        res.writeHead(404);
-        res.end("not found");
-      }
-      break;
-  }
+app.get("/", (req, res) => {
+  res.sendFile("./public/index.html", { root: __dirname });
 });
 
-const io = new Server(server, {
-  serveClient: true,
+app.use("/public", express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}`);
 });
-
-io.on("connection", (socket) => {
-  // send a message to the client
-  socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) });
-  let x = 1;
-  setInterval(function () {
-    socket.emit("new-number", x);
-    x = (x % 10) + 1;
-  }, 10000);
-
-  // receive a message from the client
-  socket.on("hello from client", (...args) => {
-    // ..
-    console.log("received hello from client: " + JSON.stringify(args));
-  });
-});
-
-server.listen(process.env.PORT || 3000);
