6bd061dec105d6f08488f90ec08a6675891c9696
Kumiszhan Dybyspayeva <d.kumiszhan@gmail.com>
2022-04-30 00:37:55 +0100

    Connected active_users.json

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const activeUsers = require("./active_users");
 
 app.get("/", (req, res) => {
   res.sendFile("./public/index.html", { root: __dirname });
 });
 
 app.use("/public", express.static("public"));
+
+app.get("/api/active_users.json", activeUsers.getData);
 
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}`);
