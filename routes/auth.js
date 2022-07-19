7351c0fcdf320df3e1748f486b5dc2c7abcc932d
Kumiszhan Dybyspayeva <d.kumiszhan@gmail.com>
2022-07-20 00:50:31 +0100

    Sort of auth working

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,6 @@
+var passport = require("passport");
+var GoogleStrategy = require("passport-google-oidc");
+
 var express = require("express");
 
 var router = express.Router();
@@ -6,4 +9,104 @@ router.get("/login", function (req, res, next) {
   res.render("login");
 });
 
+router.get("/login/federated/google", passport.authenticate("google"));
+
+router.get(
+  "/oauth2/redirect/google",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
 module.exports = router;
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, {
+      id: user.id,
+      email: user.emails[0].value,
+      name: user.displayName,
+    });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+/* 
+import mongoose from "mongoose";
+mongoose.connect('mongodb://localhost:27017/myapp');
+const { Schema } = mongoose;
+
+const dbSchema = new Schema({
+  user: {
+    email: String,
+    sessionToken: String,
+  },
+});
+
+const dbModel = mongoose.model("Model", dbSchema);
+
+dbModel.find({'issuer': 'Google'}, {'profile.id': ClientID?}, function(err, cred))
+dbModel.set()
+*/
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env["GOOGLE_CLIENT_ID"],
+      clientSecret: process.env["GOOGLE_CLIENT_SECRET"],
+      callbackURL: "http://kuma.com:3000/oauth2/redirect/google",
+      scope: "profile email",
+    },
+    function (endpoint, profile, cb) {
+      console.log("doing stuff");
+      console.log(endpoint);
+      console.log(profile);
+      console.log("finished doing stuff");
+      return cb(null, profile);
+    }
+    //   function(issuer, profile, cb) {
+    //     db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+    //       issuer,
+    //       profile.id
+    //     ], function(err, cred) {
+    //       if (err) { return cb(err); }
+    //       if (!cred) {
+    //         // The Google account has not logged in to this app before.  Create a
+    //         // new user record and link it to the Google account.
+    //         db.run('INSERT INTO users (name) VALUES (?)', [
+    //           profile.displayName
+    //         ], function(err) {
+    //           if (err) { return cb(err); }
+
+    //           var id = this.lastID;
+    //           db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+    //             id,
+    //             issuer,
+    //             profile.id
+    //           ], function(err) {
+    //             if (err) { return cb(err); }
+    //             var user = {
+    //               id: id.toString(),
+    //               name: profile.displayName
+    //             };
+    //             return cb(null, user);
+    //           });
+    //         });
+    //       } else {
+    //         // The Google account has previously logged in to the app.  Get the
+    //         // user record linked to the Google account and log the user in.
+    //         db.get('SELECT * FROM users WHERE id = ?', [ cred.user_id ], function(err, user) {
+    //           if (err) { return cb(err); }
+    //           if (!user) { return cb(null, false); }
+    //           return cb(null, user);
+    //         });
+    //       }
+    //     };
+    //   }
+  )
+);
--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 // const indexRouter = require("./public/script");
 // const authRouter = require("./routes/auth");
+const auth = require("./routes/auth");
 
 const express = require("express");
+const passport = require("passport");
 const app = express();
 const port = 3000;
 const activeUsers = require("./active_users");
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+var mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://root:root_password@localhost:27017/emily?authSource=admin"
+);
+
+// const { Schema } = mongoose;
+
+// console.log({ Schema });
+
+// const dbSchema = new Schema({
+//   user: {
+//     email: String,
+//     sessionToken: String,
+//   },
+// });
+
+// console.log(dbSchema);
+
+// const dbModel = mongoose.model("Model", dbSchema);
+// console.log(dbModel);
+
+app.use(
+  session({
+    secret: "foo",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      client: mongoose.connection.getClient(),
+    }),
+  })
+);
+
+app.use(passport.authenticate("session"));
+
+app.use("/", auth);
 
 app.get("/", (req, res) => {
   res.sendFile("./public/index.html", { root: __dirname });
